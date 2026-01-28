87b39d0c60017a49ce59e348362dcbd1eed480ad
Giovanni Schlichta <giovanni.schlichta@gmail.com>
2026-01-28 11:05:13 -0300

    version 8.0.6

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -182,7 +182,7 @@
 
 
 
-                <a href="https://www.tiktok.com/d/1/ZS91kjnrf2AY6-8bYLj/" target="_blank" rel="noopener noreferrer"
+                <a href="https://www.tiktok.com/d/1/ZS91SCDs4SJ4g-aNUWR/" target="_blank" rel="noopener noreferrer"
                     class="flex items-center gap-3
           bg-zinc-900 border border-amber-400/60
           rounded-xl px-3 py-2
@@ -299,7 +299,7 @@
                 Ainda não tem TikTok? Crie sua conta pelo convite e participe das recompensas.
             </p>
 
-            <a href="https://www.tiktok.com/d/1/ZS91N787Wh2A3-XCFA3/" target="_blank" rel="noopener noreferrer"
+            <a href="https://www.tiktok.com/d/1/ZS91SCmfJxRYV-VuiP5/" target="_blank" rel="noopener noreferrer"
                 class="w-full inline-flex items-center justify-center
                       px-4 py-2 rounded-xl border border-zinc-700
                       hover:border-amber-400 hover:text-amber-400
@@ -324,7 +324,7 @@
 
             document.getElementById('chestBtn').disabled = true;
             document.getElementById('pillBtn').disabled = true;
-            window.location.href = 'https://www.tiktok.com/d/1/ZS91kjWba3Vbj-E8jpP/';
+            window.location.href = 'https://www.tiktok.com/d/1/ZS91SCFekWtc8-8v6Cd/';
         }
 
 
