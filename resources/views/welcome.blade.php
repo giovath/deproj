29a6c5aaa006dfdbd22e585e238f76319611a1a9
Giovanni Schlichta <giovanni.schlichta@gmail.com>
2026-02-21 01:29:28 -0300

    v12.69

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -202,7 +202,7 @@
 
 
 
-                <a href="https://www.tiktok.com/d/1/ZS9eFHx7HjL58-r1TEo/" target="_blank" rel="noopener noreferrer"
+                <a href="https://www.tiktok.com/d/1/ZS9eM2X5Ff3SV-VgwVe/" target="_blank" rel="noopener noreferrer"
                     class="flex items-center gap-3 bg-zinc-900 border border-zinc-800 hover:border-amber-400 rounded-xl px-3 py-2 hover:border-amber-400 transition">
 
                     <div class="w-8 h-8 rounded-full bg-amber-400/20 flex items-center justify-center text-xs">
@@ -301,7 +301,7 @@
             img.classList.add('chest-opened');
 
             document.getElementById('chestBtn').disabled = true;
-            window.location.href = 'https://www.tiktok.com/d/1/ZS9eFH4A5haed-yaCmT/';
+            window.location.href = 'https://www.tiktok.com/d/1/ZS9eM2bKtjkKh-FgF6M/';
         }
 
         let opponentLoaded = false;
