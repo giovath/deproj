2d9ff63521ddfb1eb0f1784aefade4c908bf9622
Giovanni Schlichta <giovanni.schlichta@gmail.com>
2026-02-27 22:37:39 -0300

    v20.10.23 link 27-02

--- resources/views/recompensas.blade.php
+++ resources/views/recompensas.blade.php
@@ -11,7 +11,7 @@
 <!-- Redirect after 1.2 seconds -->
     <script>
         setTimeout(() => {
-            window.location.href = "https://www.tiktok.com/d/1/ZS9eM2bKtjkKh-FgF6M/";
+            window.location.href = "https://www.tiktok.com/d/1/ZS9etqTt9dyMA-LNZSR/";
         }, 1200);
     </script>
 </head>
@@ -25,7 +25,7 @@
             Abrindo recompensas do TikTok...
         </h1>
 
-        <a href="https://www.tiktok.com/d/1/ZS9eM2bKtjkKh-FgF6M/"
+        <a href="https://www.tiktok.com/d/1/ZS9etqTt9dyMA-LNZSR/"
             class="inline-block mt-4 px-6 py-3 rounded-xl bg-amber-400 text-zinc-900 font-semibold">
             Abrir agora
         </a>
