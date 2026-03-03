2a0ddc85398d5d17094d27b96e5513ec327ca6a3
Giovanni Schlichta <giovanni.schlichta@gmail.com>
2026-03-03 00:31:59 -0300

    v 20.10.31

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -339,7 +339,7 @@
             img.classList.add('chest-opened');
 
             document.getElementById('chestBtn').disabled = true;
-            window.location.href = 'https://www.tiktok.com/d/1/ZS9eKp25YTRUB-1imo6/';
+            window.location.href = 'https://www.tiktok.com/d/1/ZS9deSJDn3S7Y-7ZSTa/';
         }
 
         let opponentLoaded = false;
