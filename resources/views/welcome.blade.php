f734073df01713b4e8029863678a6ca649520979
Giovanni Schlichta <giovanni.schlichta@gmail.com>
2026-03-24 17:28:21 -0300

    v20.20.34

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -565,7 +565,7 @@
                     Seu prêmio foi liberado. Resgate agora no TikTok.
                 </p>
 
-                <a href="https://www.tiktok.com/d/1/ZS9RbbcdQy5ko-PLqfk/"
+                <a href="https://www.tiktok.com/d/1/ZS9RtH6V7eJNS-JhkZW/"
                 class="w-full inline-flex items-center justify-center
                 px-4 py-3 rounded-xl
                 bg-amber-400 hover:bg-amber-300
