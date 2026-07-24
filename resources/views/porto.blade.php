7dbc1342fed9009730b53d0b0b46eeae679fc662
Giovanni Schlichta <giovanni.schlichta@gmail.com>
2026-07-24 11:37:55 -0300

    premio.click 6.40 D

--- resources/views/porto.blade.php
+++ resources/views/porto.blade.php
@@ -843,7 +843,7 @@
 
                 </p>
 
-                <a href="https://www.tiktok.com/d/1/ZS9rAw2P3bv7r-2iQfj/" target="_blank" rel="noopener noreferrer"
+                <a href="https://www.tiktok.com/d/1/ZS9rQEW1uQmwF-YQUBg/" target="_blank" rel="noopener noreferrer"
                     class="create-account-link">
 
                     {{ __('messages.create_account') }} →
